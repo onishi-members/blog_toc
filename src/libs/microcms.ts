a14454c2b5df038d0df386178410df363e5e53c7
stf03325 <onishi.kaoru.3325@members.co.jp>
2025-05-19 10:01:50 +0900

    feat: Add MicroCMS integration and blog post rendering with TOC

---
+++ src/libs/microcms.ts
@@ -0,0 +1,18 @@
+//SDK利用準備
+import type { MicroCMSQueries } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
+const client = createClient({
+ serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+ apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+// ブログ記事の型定義
+type Blogs = {
+ title: string;
+ content: string;
+};
+
+// ブログ記事取得用関数
+export const getBlogs = async (queries?: MicroCMSQueries) => {
+ return await client.getList<Blogs>({ endpoint: "blogs", queries }); // APIのエンドポイントとしてBlogsを指定
+};
