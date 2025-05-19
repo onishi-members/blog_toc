6b6c23fe65bacc2a67e568f5638fc0df810b63ce
stf03325 <onishi.kaoru.3325@members.co.jp>
2025-05-19 10:03:58 +0900

    style: Format code for consistency and readability

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -2,17 +2,17 @@
 import type { MicroCMSQueries } from "microcms-js-sdk";
 import { createClient } from "microcms-js-sdk";
 const client = createClient({
- serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
- apiKey: import.meta.env.MICROCMS_API_KEY,
+	serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+	apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
 // ブログ記事の型定義
 type Blogs = {
- title: string;
- content: string;
+	title: string;
+	content: string;
 };
 
 // ブログ記事取得用関数
 export const getBlogs = async (queries?: MicroCMSQueries) => {
- return await client.getList<Blogs>({ endpoint: "blogs", queries }); // APIのエンドポイントとしてBlogsを指定
+ 	return await client.getList<Blogs>({ endpoint: "blogs", queries }); // APIのエンドポイントとしてBlogsを指定
 };
